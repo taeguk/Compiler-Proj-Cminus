2646de0148c32972c478096101fdf670d0063111
Kwanghee Choi <juice500ml@gmail.com>
2017-03-21 05:42:11 -0700

    [minor] Changes comment from TINY to C-

--- util.c
+++ util.c
@@ -1,7 +1,7 @@
 /****************************************************/
 /* File: util.c                                     */
 /* Utility function implementation                  */
-/* for the TINY compiler                            */
+/* for the C- compiler                              */
 /* Compiler Construction: Principles and Practice   */
 /* Kenneth C. Louden                                */
 /* Modified by Kwanghee Choi                        */
