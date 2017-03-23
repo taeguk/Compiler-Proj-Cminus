ec59550e5acdfc93bdfa49cd6f11d22c49a1bbb4
Taeseung Lee <taeseunglee159@gmail.com>
2017-03-22 20:16:26 -0700

    Correct indents of main.c util.c

--- main.c
+++ main.c
@@ -18,6 +18,7 @@
 #define NO_CODE FALSE
 
 #include "util.h"
+
 #if NO_PARSE
 #include "scan.h"
 #else
@@ -45,54 +46,67 @@ int TraceCode = FALSE;
 
 int Error = FALSE;
 
-main( int argc, char * argv[] )
-{ TreeNode * syntaxTree;
+int main( int argc, char * argv[] )
+{
+  TreeNode * syntaxTree;
   char pgm[120]; /* source code file name */
   if (argc != 2)
-    { fprintf(stderr,"usage: %s <filename>\n",argv[0]);
+    {
+      fprintf(stderr,"usage: %s <filename>\n",argv[0]);
       exit(1);
     }
-  strcpy(pgm,argv[1]) ;
+
+  strcpy(pgm,argv[1]);
+/*
   if (strchr (pgm, '.') == NULL)
      strcat(pgm,".tny");
+*/
   source = fopen(pgm,"r");
   if (source==NULL)
-  { fprintf(stderr,"File %s not found\n",pgm);
-    exit(1);
-  }
+    {
+      fprintf(stderr,"File %s not found\n",pgm);
+      exit(1);
+    }
+
   listing = stdout; /* send listing to screen */
   fprintf(listing,"\nTINY COMPILATION: %s\n",pgm);
+
+
 #if NO_PARSE
   while (getToken()!=ENDFILE);
 #else
   syntaxTree = parse();
-  if (TraceParse) {
-    fprintf(listing,"\nSyntax tree:\n");
-    printTree(syntaxTree);
-  }
+  if (TraceParse)
+    {
+      fprintf(listing,"\nSyntax tree:\n");
+      printTree(syntaxTree);
+    }
 #if !NO_ANALYZE
   if (! Error)
-  { if (TraceAnalyze) fprintf(listing,"\nBuilding Symbol Table...\n");
-    buildSymtab(syntaxTree);
-    if (TraceAnalyze) fprintf(listing,"\nChecking Types...\n");
-    typeCheck(syntaxTree);
-    if (TraceAnalyze) fprintf(listing,"\nType Checking Finished\n");
-  }
+    {
+      if (TraceAnalyze) fprintf(listing,"\nBuilding Symbol Table...\n");
+      buildSymtab(syntaxTree);
+      if (TraceAnalyze) fprintf(listing,"\nChecking Types...\n");
+      typeCheck(syntaxTree);
+      if (TraceAnalyze) fprintf(listing,"\nType Checking Finished\n");
+    }
 #if !NO_CODE
   if (! Error)
-  { char * codefile;
-    int fnlen = strcspn(pgm,".");
-    codefile = (char *) calloc(fnlen+4, sizeof(char));
-    strncpy(codefile,pgm,fnlen);
-    strcat(codefile,".tm");
-    code = fopen(codefile,"w");
-    if (code == NULL)
-    { printf("Unable to open %s\n",codefile);
-      exit(1);
+    {
+      char * codefile;
+      int fnlen = strcspn(pgm,".");
+      codefile = (char *) calloc(fnlen+4, sizeof(char));
+      strncpy(codefile,pgm,fnlen);
+      strcat(codefile,".tm");
+      code = fopen(codefile,"w");
+      if (code == NULL)
+      { printf("Unable to open %s\n",codefile);
+        exit(1);
+      }
+      codeGen(syntaxTree,codefile);
+      fclose(code);
     }
-    codeGen(syntaxTree,codefile);
-    fclose(code);
-  }
+
 #endif
 #endif
 #endif
