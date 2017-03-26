8911f2b1591164809fa9bf50251135fdd609cdb9
Taeseung Lee <taeseunglee159@gmail.com>
2017-03-26 04:02:54 -0700

    Modify some codes because we make a lexical analyzer for c-minus language, not c language

--- resources/keyword1.c
+++ resources/keyword1.c
@@ -1,16 +1,9 @@
-#include <stdio.h>
-extern int bb;
-typedef long long int ll;
 int main() {
-	register int i = 1;
-	signed int si = 1;
-	unsigned int ui = 1;
-	static const int super;
-	auto int a;
-	volatile float f = 123.456e1 * 123.45e02 * 1.2f * 1.2e0f * 1.2;
-	double d = 123.456;
-
-	sizeof(int);
-
-	return 0;
+	char c;
+	double d;
+	float f;
+	int i;
+	long l;
+	short s;
+	void* ptr;
 }
--- resources/keyword3.c
+++ resources/keyword3.c
@@ -1,15 +1,8 @@
 int main(int argc) {
-	
-	switch (argc) {
-		case 1: break;
-		default: break;
+	while (0) {
 	}
-LOOP:do {break;continue;}while(1);
-	while (1) {break;}
 
 	int n;
-	for (int i = 0; i < n; ++i);
-	goto LOOP;
 	if (n)
 		;
 	else
