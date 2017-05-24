2a440ad4d09f728f13876ac5a88cc469b73eca19
Taeseung Lee <taeseunglee159@gmail.com>
2017-05-24 00:22:32 -0700

    [Project 3] Add analyze files and modify main.c

--- src/main.c
+++ src/main.c
@@ -8,9 +8,9 @@
 #include "globals.h"
 
 /* set NO_PARSE to TRUE to get a scanner-only compiler */
-#define NO_PARSE FALSE
+#define NO_PARSE TRUE
 /* set NO_ANALYZE to TRUE to get a parser-only compiler */
-#define NO_ANALYZE TRUE
+#define NO_ANALYZE FALSE
 
 /* set NO_CODE to TRUE to get a compiler that does not
  * generate code
