0699e48fbf4d42663e5b2d10b415f8fc8320a30a
juice500ml <juice500ml@gmail.com>
2017-06-04 17:20:30 +0900

    DONT_OCCUR_PRINT only in DEBUG mode

--- src/log_debugger.h
+++ src/log_debugger.h
@@ -1,19 +1,22 @@
 #ifndef __LOG_DEBUG_H__
 #define __LOG_DEBUG_H__
 
-
 #ifdef  DEBUG
+
 /* file:line:function: */
-#define LOG_DEBUG(fmt, ...) fprintf(listing, "%s:%d:%s: " fmt, \
-                                           __FILE__, __LINE__, __FUNCTION__, ##__VA_ARGS__)
+  #define LOG_DEBUG(fmt, ...) fprintf(listing, "%s:%d:%s: " fmt, \
+                                      __FILE__, __LINE__, __FUNCTION__, ##__VA_ARGS__)
+  #define DONT_OCCUR_PRINT do { \
+      fprintf(listing, "%s:%d:%s: This must not occur\n", \
+              __FILE__, __LINE__, __FUNCTION__); \
+      assert(0);\
+  } while(0)
+
 #else
-#define LOG_DEBUG(fmt, ...)
+
+  #define LOG_DEBUG(fmt, ...)
+  #define DONT_OCCUR_PRINT
+
 #endif /* DEBUG */
-
-#define DONT_OCCUR_PRINT do { \
-    fprintf(listing, "%s:%d:%s: This must not occur\n", \
-                    __FILE__, __LINE__, __FUNCTION__); \
-    } while(0)
-
 
 #endif /* __LOG_DEBUG_H__ */
